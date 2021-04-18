78259161e5cd87eaa5075ddcd230b8295e62fc90
janardan <janardan.mishra@digichorus.com>
2021-04-18 11:28:30 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,46 @@ router.get('/getUserDetails', function (req, res) {
 
 
 
+
+// 1. GET api to return total like count per user from all posts in the attached dataset.
+router.get('/totalLikeCount', function (req, res) {
+
+  User.aggregate([
+    { "$unwind": "$user.uid" },
+    {
+      $group: {
+       "_id": "$user.uid",
+        "totalLike": { $sum: "$likes" }
+      }
+    }
+
+  ], function (err, result) {
+    if (err) {
+      // console.log("errr-----------------> ");
+      // console.log(err);
+      res.json({
+        "status": "ERROR",
+        "msg": "Total like count getting Issue",
+        "data": err
+      });
+    } else {
+      // console.log("Result-----------------> ");
+      // console.log(result);
+      res.json({
+        "status": "OK",
+        "msg": "Total like count fetch successfully",
+        "data": result
+      });
+
+    }
+
+  })
+})
+
+
+
+
+//2. GET api to return total like count for the user with given uid.
 router.post('/totalLikeCountByUserId', function (req, res) {
 
 // req.body.userId
@@ -68,46 +108,6 @@ router.post('/totalLikeCountByUserId', function (req, res) {
 
   })
 })
-
-
-
-router.get('/totalLikeCount', function (req, res) {
-
-  User.aggregate([
-    { "$unwind": "$user.uid" },
-    {
-      $group: {
-       "_id": "$user.uid",
-        "totalLike": { $sum: "$likes" }
-      }
-    }
-
-  ], function (err, result) {
-    if (err) {
-      // console.log("errr-----------------> ");
-      // console.log(err);
-      res.json({
-        "status": "ERROR",
-        "msg": "Total like count getting Issue",
-        "data": err
-      });
-    } else {
-      // console.log("Result-----------------> ");
-      // console.log(result);
-      res.json({
-        "status": "OK",
-        "msg": "Total like count fetch successfully",
-        "data": result
-      });
-
-    }
-
-  })
-})
-
-
-
-
 
 
 
